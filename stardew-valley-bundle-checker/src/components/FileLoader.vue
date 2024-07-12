469c636f7aaee2180aead4e06e2d48ca7635c17d
JasBitMaster <joshua.stewart@eagles.oc.edu>
2024-07-12 15:24:27 -0500

    fix: added proper credit links

--- stardew-valley-bundle-checker/src/components/FileLoader.vue
+++ stardew-valley-bundle-checker/src/components/FileLoader.vue
@@ -39,17 +39,18 @@
   color="#262626B0"
   >
   <v-col class="text-center">
-    Programmed by <a href="">JasBitMaster</a>
+    Programmed by <a href="https://github.com/JasBitMaster">JasBitMaster</a>
   </v-col>
   <v-divider class="border-opacity-100" inset="20px" thickness="2px" color="white" vertical />
   <v-col class="text-center">
-    Inspired by <a href="">Blade</a>, <a href="">ArgonMatrix</a>, and the
+    Inspired by <a href="https://www.youtube.com/@BlaDeSDV">Blade</a>,
+    <a href="https://www.youtube.com/@ArgonMatrix">ArgonMatrix</a>, and the
     <br>rest of the Stardew Valley Community
   </v-col>
   <v-divider class="border-opacity-100" inset="20px" thickness="2px" color="white" vertical />
   <v-col class="text-center">
     Stardew Valley, including all game assets,
-    <br>belongs to <a href="">ConcernedApe</a>
+    <br>belongs to <a href="https://www.stardewvalley.net/">ConcernedApe</a>
   </v-col>
   </v-footer>
 </template>
